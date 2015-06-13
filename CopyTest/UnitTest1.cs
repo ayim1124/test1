7e744850cb1a2067bd9f8b52307f622f862d2250
ayim <ayim1115@gmail.com>
2015-06-13 16:22:27 +0900

    1

--- CopyTest/UnitTest1.cs
+++ CopyTest/UnitTest1.cs
@@ -5,11 +5,11 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace CopyTest
 {
     [TestClass]
+    [DeploymentItem("test1.txt")]
+    [DeploymentItem("test2.txt")]
     public class UnitTest1
     {
         [TestMethod]
-       // [DeploymentItem("test1.txt")]
-       // [DeploymentItem("test2.txt")]
         public void ConstructorTest()
         {
             Assert.IsTrue(File.Exists("test1.txt"));
