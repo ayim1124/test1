10a002429133119f4af9e2e5fa2e789dde0bdc77
ayim <ayim1115@gmail.com>
2015-06-13 14:57:58 +0900

    first

---
+++ CopyTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CopyTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ConstructorTest()
+        {
+            Assert.IsTrue(File.Exists("test1.txt"));
+        }
+    }
+}
